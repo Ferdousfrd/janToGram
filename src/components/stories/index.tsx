7ed19b19d14b8f45e43d8ec0ddaa9a0add3fb665
Ferdousfrd <ferdous.frd81@gmail.com>
2025-01-14 23:30:58 +0200

    fixed the stories, now all the user display photos will be shown instories bar instead of statitc imgs

--- src/components/stories/index.tsx
+++ src/components/stories/index.tsx
@@ -2,36 +2,48 @@ import * as React from "react";
 import img1 from "@/assets/images/img1.jpg";
 import img2 from "@/assets/images/img2.jpg";
 import img3 from "@/assets/images/img3.jpg";
+import { getAllUsers } from "@/repository/user.service";
+import { ProfileResponse } from "@/types";
+import { useUserAuth } from "@/context/userAuthContext";
 
 interface IStoriesProps {
   // Add props here if needed
 }
 
 const Stories: React.FunctionComponent<IStoriesProps> = () => {
-  return (
-    <div className="flex justify-between">
-      <img
-        src={img1}
-        className="w-20 h-20 rounded-full border-4 border-slate-800 object-cover"
-      />
-      <img
-        src={img2}
-        className="w-20 h-20 rounded-full border-4 border-slate-800 object-cover"
-      />
-      <img
-        src={img3}
-        className="w-20 h-20 rounded-full border-4 border-slate-800 object-cover"
-      />
-       <img
-        src={img1}
-        className="w-20 h-20 rounded-full border-4 border-slate-800 object-cover"
-      />
-       <img
-        src={img1}
-        className="w-20 h-20 rounded-full border-4 border-slate-800 object-cover"
-      />
-    </div>
+  const { user } = useUserAuth();
+  const [suggestedUser, setSuggestedUser] = React.useState<ProfileResponse[]>(
+    []
   );
+
+  const getSuggestedUsers = async (userId: string) => {
+    const response = (await getAllUsers(userId)) || [];
+    console.log("the Suggetsed Users response is ", response);
+    setSuggestedUser(response);
+  };
+
+  React.useEffect(() => {
+    if (user?.uid != null) {
+      getSuggestedUsers(user.uid);
+    }
+  }, []);
+
+  const renderUsers = () => {
+    return suggestedUser.map((item) => {
+      return (
+        <div className="flex flex-row items-center mb-4 border-gray-400 justify-start">
+          <span className="mr-2">
+            <img
+              src={item?.photoURL ? item.photoURL : img1}
+              className="w-20 h-20 rounded-full border-2 border-slate-800 object-cover mr-4"
+            />
+          </span>
+        </div>
+      );
+    });
+  };
+
+  return <div className="flex justify-start">{renderUsers()}</div>;
 };
 
 export default Stories;
